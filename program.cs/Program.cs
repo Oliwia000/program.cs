b60ce281c0dddee4f3e03895f25e2a08cfd11998
Oliwia000 <152185830+Oliwia000@users.noreply.github.com>
2024-04-19 10:38:15 +0200

    Dodaj pliki projektów.

---
+++ program.cs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+internal class Program
+{
+    static Sortownia sortownia;
+    static void Main(string[] args)
+    {
+        sortownia = new Sortownia();
+        Console.ReadKey();
+    }
+
+    private int SzukajIndeksuMaxa(int j)
+    {
+        int id = j;
+        for (int k = j; k < t.Length; k++)
+            if (t[k] > t[id]) id = k;
+        return id;
+    }
+
+    
+}
+
+    //*void selection_sort(int tab[], int i)
+      //  int mn_index;
+       // for (int z = 0; i < i - 1; i++)
+         //   for (int j = i + 1; j < i; j++)
+           //     if (tab[] < tab[mn_index])
+            //        mn_index = j;
+         //   mn_index = i;
+          //  for (int z2 = 0; i < i - 1; i++) ;
+ 
+
+
+/************************************* 
+* nazwa funkcji: <> 
+* parametry wejściowe:<> - <> 
+* wartość zwracana:  <> 
+* autor: <> 
+*************************************/
---
+++ program.cs/Sort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program.cs
+{
+    internal class Sort
+    {
+        private int[] t = new int[10];
+        public Sort()
+        {
+            Console.WriteLine("Podaj 10 liczb całkowitych");
+            int licznik = 0;
+            do
+            {
+                Console.WriteLine((licznik + 1).ToString() + ":");
+                t[licznik] = int.Parse(Console.ReadLine());
+                licznik++;
+            } while (licznik < 10);
+
+        }
+    }
+
+}

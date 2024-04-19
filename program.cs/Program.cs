99aa8aa90a95d22d06c1b53e00dea0c03ec78e3c
Oliwia000 <152185830+Oliwia000@users.noreply.github.com>
2024-04-19 11:15:45 +0200

    <zmiana>

--- program.cs/Program.cs
+++ program.cs/Program.cs
@@ -2,38 +2,16 @@
 
 internal class Program
 {
-    static Sortownia sortownia;
+    static Sort sortownia;
     static void Main(string[] args)
     {
-        sortownia = new Sortownia();
+        sortownia = new Sort();
+        Console.WriteLine("Pokaż przed sortowaniem: ");
+        sortownia.PokazTablice();
+        sortownia.Sortuj();
+        Console.WriteLine("\nPokaż po sortowaniu: ");
+        sortownia.PokazTablice();
         Console.ReadKey();
     }
 
-    private int SzukajIndeksuMaxa(int j)
-    {
-        int id = j;
-        for (int k = j; k < t.Length; k++)
-            if (t[k] > t[id]) id = k;
-        return id;
-    }
-
-    
 }
-
-    //*void selection_sort(int tab[], int i)
-      //  int mn_index;
-       // for (int z = 0; i < i - 1; i++)
-         //   for (int j = i + 1; j < i; j++)
-           //     if (tab[] < tab[mn_index])
-            //        mn_index = j;
-         //   mn_index = i;
-          //  for (int z2 = 0; i < i - 1; i++) ;
- 
-
-
-/************************************* 
-* nazwa funkcji: <> 
-* parametry wejściowe:<> - <> 
-* wartość zwracana:  <> 
-* autor: <> 
-*************************************/
--- program.cs/Sort.cs
+++ program.cs/Sort.cs
@@ -3,24 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace program.cs
-{
     internal class Sort
     {
         private int[] t = new int[10];
+
         public Sort()
         {
-            Console.WriteLine("Podaj 10 liczb całkowitych");
+            Console.WriteLine("Podaj 10 liczb całkowitych ");
             int licznik = 0;
             do
             {
-                Console.WriteLine((licznik + 1).ToString() + ":");
+                Console.WriteLine((licznik + 1).ToString() + ": ");
                 t[licznik] = int.Parse(Console.ReadLine());
                 licznik++;
             } while (licznik < 10);
 
         }
-    }
-
+    /************************************* 
+    * nazwa funkcji: <Sort> 
+    * parametry wejściowe:<> - <> 
+    * wartość zwracana:  <> 
+    *************************************/
+    public void PokazTablice()
+        {
+            for (int i = 0; i < t.Length; i++)
+                Console.WriteLine(String.Format("{0}", t[i]));
+        }
+    /************************************* 
+    * nazwa funkcji: <PokazTablice> 
+    * parametry wejściowe:<> - <> 
+    * wartość zwracana:  <> 
+    *************************************/
+    private int SzukajIndeksuMaxa(int j)
+        {
+            int id = j;
+            for (int k = j; k < t.Length; k++)
+                if (t[k] > t[id]) id = k;
+            return id;
+        }
+    /************************************* 
+    * nazwa funkcji: <SzukajIndeksuMaxa> 
+    * parametry wejściowe:<> - <> 
+    * wartość zwracana:  <> 
+    *************************************/
+    public void Sortuj()
+        {
+            for (int i = 0; i < t.Length; i++)
+            {
+                int id = SzukajIndeksuMaxa(i);
+                int bufor = t[i];
+                t[i] = t[id];
+                t[id] = bufor;
+            }
+        }
 }
+
+    /************************************* 
+    * nazwa funkcji: <Sortuj> 
+    * parametry wejściowe:<> - <> 
+    * wartość zwracana:  <> 
+    * autor: <Oliwia Strzelczyk 3d> 
+    *************************************/
